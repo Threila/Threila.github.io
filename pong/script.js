9bac452d3356eed67bac59e52b5f9175bd959789
Threila <122054049+Threila@users.noreply.github.com>
2023-08-09 16:41:16 +0200

    resizable

--- pong/script.js
+++ pong/script.js
@@ -2,6 +2,8 @@ const game= document.getElementById("game");
 const player1= document.getElementById("player1");
 const player2= document.getElementById("player2");
 const ball= document.getElementById("ball");
+const part1= document.getElementById("part1");
+const part2= document.getElementById("part2");
 let play=false;
 let end=true;
 var map = [];
@@ -9,6 +11,39 @@ let vector=[10,10]
 score1=0;
 score2=0;
 it=0;
+height=window.innerHeight;
+width=window.innerWidth;
+paddleSize=height/4.7;
+paddleWidth=width/50;
+initialSpeed=width/200;
+borderLeft=width/25+paddleSize-(width/13);
+borderRight=width-width/25-paddleWidth-30;
+console.log(width);
+console.log(height);
+initialStartup();
+function initialStartup(){
+    game.style.width=width+"px";
+    game.style.height=height+"px";
+    player1.style.height=paddleSize+"px";
+    player2.style.height=paddleSize+"px";
+    player2.style.width=paddleWidth+"px";
+    player1.style.width=paddleWidth+"px";
+    player1.style.marginTop=(height/2-paddleSize/2)+"px";
+    player2.style.marginTop=(height/2-paddleSize/2)+"px";
+    player1.style.marginLeft=width/25 +"px";
+    player2.style.marginLeft=(width/2-width/25-paddleWidth) +"px";
+    part1.style.height=height+"px";
+    part2.style.height=height+"px";
+    part2.style.width=width/2+"px";
+    part1.style.width=width/2+"px";
+    ball.style.marginLeft=width/2+"px";
+    ball.style.marginTop=height/2*-1+"px";
+    document.getElementById("win").style.marginTop=(height/2)*-1+"px";
+    document.getElementById("win").style.marginLeft=width/2-500+"px";
+    document.getElementById("points").style.marginTop=(height/2+height/4)*-1+"px";
+    document.getElementById("points").style.marginLeft=width/2-200+"px";
+
+}
 onkeydown = onkeyup = function(e){
     
     e = e || event; 
@@ -45,28 +80,28 @@ function input(){
     if(map[87]){
         //console.log("a")
         if(characterTop1>0){
-            player1.style.marginTop=(characterTop1-10)+ "px";
+            player1.style.marginTop=(characterTop1-initialSpeed)+ "px";
             }
         
        }
        if(map[83]){
         
-            if(characterTop1<630){
-            player1.style.marginTop=(characterTop1+10)+ "px";
+            if(characterTop1<=(height-paddleSize-10)){
+            player1.style.marginTop=(characterTop1+initialSpeed)+ "px";
         }
        }
     
        if(map[38]){
         //console.log("a")
         if(characterTop2>0){
-            player2.style.marginTop=(characterTop2-10)+ "px";
+            player2.style.marginTop=(characterTop2-initialSpeed)+ "px";
             }
         
        }
        if(map[40]){
         
-            if(characterTop2<630){
-            player2.style.marginTop=(characterTop2+10)+ "px";
+            if(characterTop2<=(height-paddleSize-10)){
+            player2.style.marginTop=(characterTop2+initialSpeed)+ "px";
         }
        }
 }
@@ -75,20 +110,21 @@ function updateBall(){
 let x=parseInt(window.getComputedStyle(ball).marginLeft);
 let y=parseInt(window.getComputedStyle(ball).marginTop);
 
-if (y>=-25||y<-790){
+if (y>=(-1*height/23.175-10)||y<=-height){
     vector[1]=vector[1]*-1;
 
 }
 var characterTop1 =parseInt(window.getComputedStyle(player1).marginTop);
 var characterTop2 =parseInt(window.getComputedStyle(player2).marginTop);
 if(temp){
-if ((x<100)&&(y>(-1*(800-characterTop1))&&y<((800-characterTop1-170)*-1))){
+if ((x<borderLeft)&&(y>(-1*(height-characterTop1))&&y<((height-characterTop1-paddleSize)*-1))){
     vector[0]=vector[0]*-1;
+    vector[0]=vector[0]+0.5;
     temp=false;
 }
-    else if(x>1370&&(y>(-1*(800-characterTop2))&&y<((800-characterTop2-170)*-1))){
+    else if(x>borderRight&&(y>(-1*(height-characterTop2))&&y<((height-characterTop2-paddleSize)*-1))){
         vector[0]=vector[0]*-1;
-        vector[0]=vector[0]-1;
+        vector[0]=vector[0]-0.5;
         temp=false;
 }
 }else{
@@ -96,9 +132,9 @@ if ((x<100)&&(y>(-1*(800-characterTop1))&&y<((800-characterTop1-170)*-1))){
 }
 ball.style.marginLeft=(x+vector[0])+"px";
 ball.style.marginTop=(y+vector[1])+"px";
-if(x<40){
+if(x<borderLeft-(width/56,35)){
     rebuild();
-    vector[0]=10;
+    vector[0]=initialSpeed;
     play=false;
     score2++;
     document.getElementById(2).innerHTML=score2;
@@ -107,10 +143,10 @@ if(x<40){
         document.getElementById("win").innerHTML="Player 2 wins!!!";
     }
 }
-if(x>1430){
+if(x>borderRight+(width/56,35)){
     
     rebuild();
-    vector[0]=-10;
+    vector[0]=-1*initialSpeed;
     play=false;
     score1++;
     document.getElementById(1).innerHTML=score1;
@@ -122,8 +158,8 @@ if(x>1430){
 }
 function rebuild(){
 
-    ball.style.marginTop="-400px";
-    ball.style.marginLeft="740px";
+    ball.style.marginLeft=width/2+"px";
+    ball.style.marginTop=height/2*-1+"px";
     
     vector[1]=getRandomInt(10)+1;
     if(getRandomInt(2)==0){
